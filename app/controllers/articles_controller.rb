3be1ee7e28fce72caa6659ac67cd46e2487dc73d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-12-14 00:40:23 -0500

    created edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	before_action :set_article, only: %i[show]
+	before_action :set_article, only: %i[show edit update]
 
 	def index
 		@articles = Article.all
@@ -7,9 +7,37 @@ class ArticlesController < ApplicationController
 
 	def show; end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = 'Article was created successfully'
+			redirect_to article_path(@article)
+		else
+			render :new
+		end
+	end
+
+	def edit; end
+
+	def update
+		if @article.update(article_params)
+			flash[:notice] = 'Article was updated successfully'
+			redirect_to article_path(@article)
+		else
+			render :edit
+		end
+	end
+
 	private
 
 	def set_article
 		@article = Article.find(params[:id])
+	end
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 end
