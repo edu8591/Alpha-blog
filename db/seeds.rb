12aeea201cfed16981c159d956077ada8cc2326c
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-03 17:29:42 -0500

    adds seeds for users and articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'destroying all previous users'
+User.destroy_all
+puts 'All users destroyed successfully'
+puts 'Creating 30 Users'
+count = 1
+30.times do
+	username = Faker::Name.first_name
+	puts "creating user ##{count} username: #{username}"
+	count += 1
+
+	user =
+		User.create!(
+			username: username,
+			email:
+				"#{username.delete(' ') if username.include?(' ') || username}@example.com",
+			password: 'password',
+		)
+
+	(1..9).to_a.sample.times do
+		Article.create!(
+			title: Faker::Books::Lovecraft.tome,
+			description: Faker::Books::Lovecraft.sentence,
+			user: user,
+		)
+	end
+end
