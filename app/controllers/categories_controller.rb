14b8034fa422d6e3eb284d24ab8bf6ec849177f2
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-04 13:17:26 -0500

    adds category creation form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,25 @@
 class CategoriesController < ApplicationController
-	def new; end
+	def new
+		@category = Category.new
+	end
 
-	def create; end
+	def create
+		@category = Category.new(category_params)
+		if @category.save
+			flash[:notice] = 'Category was successfully created'
+			redirect_to category_path(@category)
+		else
+			render 'new'
+		end
+	end
 
 	def show; end
 
 	def index; end
+
+	private
+
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
