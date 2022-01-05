65663316419a3dd3f2574373736ed966ad0081a7
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-04 22:24:51 -0500

    creates the article_Categories table and made associations

--- app/models/Category.rb
+++ app/models/Category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+	has_many :article_categories
+	has_many :articles, through: :article_categories
 	validates :name, presence: true
 	validates :name, uniqueness: true
 	validates :name, length: { in: 3..20 }
