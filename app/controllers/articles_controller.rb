920d329d3319e1585899bd950bf2d81fd0dcc2fc
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-01-04 23:12:28 -0500

    adds category select on article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
+		byebug
 		@article = Article.new(article_params)
 		@article.user = current_user
 		if @article.save
@@ -46,7 +47,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def article_params
-		params.require(:article).permit(:title, :description)
+		params.require(:article).permit(:title, :description, category_ids: [])
 	end
 
 	def require_same_user
